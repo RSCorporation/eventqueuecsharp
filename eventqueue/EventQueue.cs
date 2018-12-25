97554b645ccc29990b30306e65675e454c279eb9
Roman <roma-svistunov@tut.by>
2018-12-25 14:07:48 +0300

    removing

--- eventqueue/EventQueue.cs
+++ eventqueue/EventQueue.cs
@@ -5,16 +5,18 @@ namespace eventqueue
 {
     internal class EventTimePair : IComparable, IComparable<EventTimePair>
     {
-        internal EventTimePair(TimerCallback action, object state, DateTime time)
+        internal EventTimePair(TimerCallback action, object state, DateTime time, Guid guid)
         {
             this.Action = action ?? throw new ArgumentNullException(nameof(action));
             this.Time = time;
             this.State = state;
+            this.Guid = guid;
         }
 
         internal TimerCallback Action { get; }
         internal DateTime Time { get; }
         internal object State { get; }
+        internal Guid Guid { get; }
 
         public int CompareTo(EventTimePair other)
         {
@@ -37,20 +39,29 @@ namespace eventqueue
 
         private Timer timer;
         private SortedSet<EventTimePair> set;
-        public void AddEvent(TimerCallback action, object state, DateTime time)
+        private SortedSet<Guid> removedEvents;
+        public Guid AddEvent(TimerCallback action, object state, DateTime time)
         {
             if (time == null) throw new ArgumentNullException(nameof(time));
             if (action == null) throw new ArgumentNullException(nameof(action));
             if (time < DateTime.Now) throw new ArgumentException("time can not be before now", nameof(time));
-            lock (set) set.Add(new EventTimePair(action, state, time));
+            Guid g = Guid.NewGuid();
+            lock (set) set.Add(new EventTimePair(action, state, time, g));
             if (time < set.Min.Time)
             {
                 timer.Change((time - DateTime.Now).Milliseconds, Timeout.Infinite);
             }
+            return g;
+        }
+        public void RemoveEvent(Guid guid)
+        {
+            removedEvents.Add(guid);
         }
         private void Callback(object state)
         {
             SortedSet<EventTimePair> curr = (SortedSet<EventTimePair>)state;
+            while (curr.Count > 0 && removedEvents.Contains(curr.Min.Guid)) curr.Remove(curr.Min);
+            if (curr.Count == 0) return;
             curr.Min.Action.Invoke(curr.Min.State);
             lock(curr) curr.Remove(curr.Min);
 
