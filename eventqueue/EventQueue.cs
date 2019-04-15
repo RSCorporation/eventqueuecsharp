92b50ab5c9bd5a82d37f83f67476d55433362c47
Roman <roma-svistunov@tut.by>
2019-04-15 17:18:31 +0300

    Fixes + Refactoring
    Co-Authored-By: Egor Koleda <radioegor146@users.noreply.github.com>

--- eventqueue/EventQueue.cs
+++ eventqueue/EventQueue.cs
@@ -2,38 +2,63 @@
 using System.Collections.Generic;
 using System.Collections.Concurrent;
 using System.Linq;
-using System.Text;
 using System.Threading;
-using System.Threading.Tasks;
 
-namespace DynNetLib.Util
+namespace DnmpLibrary.Util
 {
-    internal static class EventQueue
+    public static class EventQueue
     {
         private class EventQueueEvent : IComparable<EventQueueEvent>
         {
             public readonly TimerCallback Action;
             public readonly DateTime CallTime;
+            public readonly Guid Guid;
             public readonly object State;
 
-            public EventQueueEvent(TimerCallback action, object state, DateTime callTime)
+            public EventQueueEvent(TimerCallback action, object state, DateTime callTime, Guid guid)
             {
                 Action = action;
                 CallTime = callTime;
                 State = state;
+                Guid = guid;
             }
 
             public int CompareTo(EventQueueEvent other)
             {
                 if (this == other)
                     return 0;
-                return CallTime < other.CallTime ? -1 : 1;
+                return CallTime == other.CallTime ? Guid.GetHashCode() - other.Guid.GetHashCode() : CallTime < other.CallTime ? -1 : 1;
             }
         }
-
-        private static readonly Timer timer = new Timer(Callback);
+        
         private static readonly SortedSet<EventQueueEvent> events = new SortedSet<EventQueueEvent>();
         private static readonly ConcurrentDictionary<Guid, EventQueueEvent> eventById = new ConcurrentDictionary<Guid, EventQueueEvent>();
+
+        static EventQueue()
+        {
+            var worker = new Thread(() =>
+            {
+                for (;;)
+                {
+                    EventQueueEvent minEvent;
+                    lock (events)
+                        minEvent = events.Any() ? events.Min: null;
+                    if (minEvent != null && minEvent.CallTime <= DateTime.Now)
+                    {
+                        lock (events)
+                            events.Remove(minEvent);
+                        eventById.TryRemove(minEvent.Guid, out var _);
+                        minEvent.Action.BeginInvoke(minEvent.State, ActionInvokeCallback, new InvokeStateObject
+                        {
+                            TimerCallback = minEvent.Action
+                        });
+                    }
+                    Thread.Sleep(1);
+                }
+                // ReSharper disable once FunctionNeverReturns
+            });
+            worker.Start();
+        }
 
         public static Guid AddEvent(TimerCallback action, object state, DateTime time)
         {
@@ -42,13 +67,11 @@ namespace DynNetLib.Util
 
         public static Guid AddEvent(TimerCallback action, object state, DateTime callTime, Guid newId)
         {
-            var newEvent = new EventQueueEvent(action, state, callTime);
-            while (!eventById.TryAdd(newId, newEvent))
-                Thread.Sleep(1);
+            var newEvent = new EventQueueEvent(action, state, callTime, newId);
+            eventById.TryAdd(newId, newEvent);
             lock (events)
             {
                 events.Add(newEvent);
-                timer.Change(ClampTimeSpan(events.Min.CallTime - DateTime.Now), Timeout.InfiniteTimeSpan);
             }
             return newId;
         }
@@ -57,40 +80,16 @@ namespace DynNetLib.Util
         {
             if (!eventById.ContainsKey(id))
                 return;
-            EventQueueEvent idEvent;
-            while (!eventById.TryRemove(id, out idEvent)) Thread.Sleep(1);
+            eventById.TryRemove(id, out var idEvent);
             lock (events)
             {
                 events.Remove(idEvent);
-                timer.Change(events.Count > 0 ? ClampTimeSpan(events.Min.CallTime - DateTime.Now) : Timeout.InfiniteTimeSpan, Timeout.InfiniteTimeSpan);
             }
         }
 
         private class InvokeStateObject
         {
             public TimerCallback TimerCallback;
-        }
-
-        private static TimeSpan ClampTimeSpan(TimeSpan original)
-        {
-            return original.TotalMilliseconds < 0 ? TimeSpan.Zero : original;
-        }
-
-        private static void Callback(object state)
-        {
-            EventQueueEvent minEvent;
-
-            lock (events)
-            {
-                minEvent = events.Min;
-                events.Remove(minEvent);
-                timer.Change(events.Count > 0 ? ClampTimeSpan(events.Min.CallTime - DateTime.Now) : Timeout.InfiniteTimeSpan, Timeout.InfiniteTimeSpan);
-            }
-
-            minEvent.Action.BeginInvoke(minEvent.State, ActionInvokeCallback, new InvokeStateObject
-            {
-                TimerCallback = minEvent.Action
-            });
         }
 
         private static void ActionInvokeCallback(IAsyncResult asyncResult)
